374f32b2e58af328b74b77693389a19b775be6e8
Nico Rehwaldt <git_nikku@nixis.de>
2021-12-18 14:38:35 +0100

    feat: initial commit
    * use `file-drops` compatible API
    * rename to `file-open`

--- README.md
+++ README.md
@@ -1,39 +1,35 @@
-# open-file-dialog [![stability][0]][1]
-[![npm version][2]][3] [![downloads][4]][5] [![js-standard-style][6]][7]
+# file-open
 
-Opens the browsers file dialog
+Opens a file from the browser.
+
 
 ## Usage
+
 ```js
-var dialog = require('./')
+import { fileOpen } from 'file-open';
 
-var btn = document.createElement('button')
-btn.innerText = 'Upload images'
-btn.addEventListener('click', function () {
-  dialog({
-    multiple: true,
-    accept: 'image/*'
-  }, function (images) {
-    console.log(images)
-  })
-})
+const button = document.querySelector('#chooser-button');
 
-document.body.appendChild(btn)
+button.addEventListener('click', async function() {
+  const files = await fileOpen();
+
+  console.log(files);
+});
 ```
 
 ## API
-### `dialog(opts, cb)`
+
+### `fileOpen(opts) => Promise<File[]>`
+
 - `opts.multiple` Allows selection of multiple files
 - `opts.accept` Specifies allowed file extensions or MIME types. [more](https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/file#Limiting_accepted_file_types)
 
-## License
-[MIT](https://tldrlegal.com/license/mit-license)
 
-[0]: https://img.shields.io/badge/stability-experimental-orange.svg?style=flat-square
-[1]: https://nodejs.org/api/documentation.html#documentation_stability_index
-[2]: https://img.shields.io/npm/v/open-file-dialog.svg?style=flat-square
-[3]: https://npmjs.org/package/open-file-dialog
-[4]: http://img.shields.io/npm/dm/open-file-dialog.svg?style=flat-square
-[5]: https://npmjs.org/package/open-file-dialog
-[6]: https://img.shields.io/badge/code%20style-standard-brightgreen.svg?style=flat-square
-[7]: https://github.com/feross/standard
+## See also
+
+* [file-drops](https://github.com/nikku/file-drops) - simple browser file drop utility
+
+
+## License
+
+MIT
--- index.js
+++ index.js
@@ -1,21 +1,104 @@
-module.exports = dialog
+export function fileOpen(opts = {}) {
 
-function dialog (opts, cb) {
-  var input = document.createElement('input')
+  let resolve, reject;
 
-  if (opts.multiple) input.setAttribute('multiple', '')
-  if (opts.accept) input.setAttribute('accept', opts.accept)
-  input.setAttribute('type', 'file')
-  input.style.display = 'none'
+  const promise = new Promise((_resolve, _reject) => {
+    resolve = _resolve;
+    reject = _reject;
+  });
 
-  input.addEventListener('change', function (e) {
-    cb(input.files)
-    input.remove()
-  })
+  const input = document.createElement('input');
 
-  document.body.appendChild(input)
+  if (opts.multiple) {
+    input.setAttribute('multiple', '');
+  }
 
-  var event = document.createEvent('MouseEvent')
-  event.initMouseEvent('click', false, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null)
-  input.dispatchEvent(event)
+  if (opts.accept) {
+    input.setAttribute('accept', opts.accept);
+  }
+
+  input.setAttribute('type', 'file');
+  input.style.display = 'none';
+
+  input.addEventListener('change', function() {
+    asyncMap(input.files, readFile, function(err, files) {
+
+      console.log(files);
+
+      if (err) {
+        reject(err);
+      } else {
+        resolve(files);
+      }
+
+      input.remove();
+    });
+
+  });
+
+  document.body.appendChild(input);
+
+  const event = document.createEvent('MouseEvent');
+  event.initMouseEvent('click', false, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
+  input.dispatchEvent(event);
+
+  return promise;
+}
+
+
+// helpers ///////////////
+
+function readFile(file, done) {
+
+  if (!window.FileReader) {
+    return done();
+  }
+
+  var reader = new FileReader();
+
+  // closure to capture the file information.
+  reader.onload = function(e) {
+
+    done(null, {
+      name: file.name,
+      path: file.path,
+      contents: e.target.result
+    });
+  };
+
+  reader.onerror = function(event) {
+    done(event.target.error);
+  };
+
+  // read in the image file as a data URL.
+  reader.readAsText(file);
+}
+
+
+function asyncMap(elements, iterator, done) {
+
+  var idx = 0,
+      results = [];
+
+  function next() {
+
+    if (idx === elements.length) {
+      done(null, results);
+    } else {
+
+      iterator(elements[idx], function(err, result) {
+
+        if (err) {
+          return done(err);
+        } else {
+          results[idx] = result;
+          idx++;
+
+          next();
+        }
+      });
+    }
+  }
+
+  next();
 }
